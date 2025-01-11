ecab538c94fd1fc9ffd172a85ee3afd21b3e906c
keklick1337 <86933883+keklick1337@users.noreply.github.com>
2025-01-11 22:04:26 +0400

    pthread.h -> adv_thread in test

--- tests/queue_test.c
+++ tests/queue_test.c
@@ -2,13 +2,16 @@
  * by Vladislav Tislenko aka keklick1337 (2025)
  * queue_test.c
  *
- * Simple test of the FIFO queue usage in C99.
+ * Simple test of the FIFO queue usage in C99, now using our "adv_thread.h"
+ * instead of direct pthreads.
+ *
  * Demonstrates both basic and thread-safe operations.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+/* Instead of <pthread.h>, include our adv_thread */
+#include "adv_thread.h"
 #include "queue.h"
 
 #define NUM_THREADS 4
@@ -24,6 +27,7 @@ typedef struct {
 
 /*
  * Producer thread: pushes several integers into the queue.
+ * Note: must match signature: void* func(void*).
  */
 void* producer_thread(void* arg) {
     ThreadArg* p = (ThreadArg*)arg;
@@ -80,29 +84,34 @@ int main(void) {
 
     /*
      * 2. Multithreaded test: multiple producers and consumers
+     *    now using our AdvThread from adv_thread.h
      */
-    pthread_t producers[NUM_THREADS];
-    pthread_t consumers[NUM_THREADS];
+    AdvThread producers[NUM_THREADS];
+    AdvThread consumers[NUM_THREADS];
     ThreadArg args[NUM_THREADS];
 
     // Create producer threads
     for (int i = 0; i < NUM_THREADS; i++) {
         args[i].queue = q;
         args[i].thread_id = i;
-        pthread_create(&producers[i], NULL, producer_thread, &args[i]);
+        if (thread_create(&producers[i], producer_thread, &args[i]) != 0) {
+            printf("Failed to create producer thread %d\n", i);
+        }
     }
 
     // Create consumer threads
     for (int i = 0; i < NUM_THREADS; i++) {
         args[i].queue = q;
         args[i].thread_id = i + 100; // just to distinguish
-        pthread_create(&consumers[i], NULL, consumer_thread, &args[i]);
+        if (thread_create(&consumers[i], consumer_thread, &args[i]) != 0) {
+            printf("Failed to create consumer thread %d\n", i + 100);
+        }
     }
 
     // Join all threads
     for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(producers[i], NULL);
-        pthread_join(consumers[i], NULL);
+        thread_join(&producers[i]);
+        thread_join(&consumers[i]);
     }
 
     printf("Queue size after all threads finished: %zu\n", queue_size(q));
